7ae0a3216b80cb2b8268b79c3018d18aef48265a
apexCoder1 <imgrimm0@gmail.com>
2025-06-30 18:58:51 -0400

    Update game.js to input the gameboard parameter

--- game.js
+++ game.js
@@ -2,4 +2,4 @@ import { drawBoard } from './draw.js';
 const gameRows = 20; 
 const gameColumns = 10; 
 const gameBoard = Array.from({ length: gameRows }, () => Array(gameColumns).fill("black"));
-drawBoard()
+drawBoard(gameBoard)
